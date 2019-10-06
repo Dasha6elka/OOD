3684c8745d90c9f7ddbcdeefef4cbd1a5f94c691
Dasha6elka <CB4kot@gmail.com>
2019-10-06 21:12:48 +0300

    Добавление выделения фигур

--- src/main/java/canvas/J2DCanvas.java
+++ src/main/java/canvas/J2DCanvas.java
@@ -10,11 +10,14 @@ import java.util.List;
 import java.util.ListIterator;
 
 import static java.awt.Cursor.HAND_CURSOR;
+import static java.awt.RenderingHints.KEY_ANTIALIASING;
+import static java.awt.RenderingHints.VALUE_ANTIALIAS_ON;
 import static java.awt.event.InputEvent.BUTTON3_DOWN_MASK;
 import static java.awt.event.MouseEvent.BUTTON1;
 
 public class J2DCanvas extends JComponent implements Canvas {
     private transient List<Item> items;
+    private transient Item selection;
 
     public J2DCanvas() {
         items = new ArrayList<>();
@@ -50,12 +53,21 @@ public class J2DCanvas extends JComponent implements Canvas {
         g.setColor(Color.LIGHT_GRAY);
         g.fillRect(0, 0, getWidth(), getHeight());
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
         for (Item item : items) {
             AffineTransform saved = g2d.getTransform();
             g2d.transform(item.transform);
             g2d.setColor(item.color);
             g2d.fill(item.shape);
+            g2d.setTransform(saved);
+        }
+        if (selection != null) {
+            AffineTransform saved = g2d.getTransform();
+            g2d.transform(selection.transform);
+            var bounds = selection.shape.getBounds();
+            g2d.setColor(Color.BLUE);
+            g2d.setStroke(new BasicStroke(2));
+            g2d.drawRect(bounds.x - 2, bounds.y - 2, bounds.width + 2, bounds.height + 2);
             g2d.setTransform(saved);
         }
     }
@@ -91,6 +103,9 @@ public class J2DCanvas extends JComponent implements Canvas {
                 AffineTransform transform = AffineTransform.getTranslateInstance(tx, ty);
                 transform.concatenate(hoverItem.transform);
                 hoverItem.transform = transform;
+                if (selection != null) {
+                    selection.transform = transform;
+                }
                 repaint();
             }
             previous = new Point(e.getPoint());
@@ -133,7 +148,35 @@ public class J2DCanvas extends JComponent implements Canvas {
             if (e.getButton() == BUTTON1 && !isDragging && hoverItem != null) {
                 isDragging = true;
                 previous = new Point(e.getPoint());
+                var item = findItem(e.getPoint());
+                if (item != null) {
+                    Rectangle itemBounds = item.shape.getBounds();
+                    Point2D itemBottomRight = item.transform.transform(new Point(itemBounds.x + itemBounds.width, itemBounds.y + itemBounds.height), null);
+                    Point2D itemTopLeft = item.transform.transform(new Point(itemBounds.x, itemBounds.y), null);
+                    double bottomRightX = itemBottomRight.getX();
+                    double bottomRightY = itemBottomRight.getY();
+                    if (selection != null) {
+                        Rectangle selectionBounds = selection.shape.getBounds();
+                        Point2D selectionBottomRight = selection.transform.transform(new Point(selectionBounds.x + selectionBounds.width, selectionBounds.y + selectionBounds.height), null);
+                        Point2D selectionTopLeft = selection.transform.transform(new Point(selectionBounds.x, selectionBounds.y), null);
+                        double selectionTopLeftX = Math.min(selectionTopLeft.getX(), itemTopLeft.getX());
+                        double selectionTopLeftY = Math.min(selectionTopLeft.getY(), itemTopLeft.getY());
+                        itemTopLeft.setLocation(selectionTopLeftX, selectionTopLeftY);
+                        bottomRightX = Math.max(itemBottomRight.getX(), selectionBottomRight.getX());
+                        bottomRightY = Math.max(itemBottomRight.getY(), selectionBottomRight.getY());
+                    }
+                    Rectangle2D.Double rec = new Rectangle2D.Double(
+                            itemTopLeft.getX(),
+                            itemTopLeft.getY(),
+                            bottomRightX - itemTopLeft.getX(),
+                            bottomRightY - itemTopLeft.getY()
+                    );
+                    selection = new Item(rec, Color.BLUE, AffineTransform.getTranslateInstance(0, 0));
+                }
+            } else {
+                selection = null;
             }
+            repaint();
         }
 
         @Override
